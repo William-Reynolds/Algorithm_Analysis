9490ae22560c4a5cb51f90441c9fd94b651773ee
William Reynolds <71613781+William-Reynolds@users.noreply.github.com>
2021-06-08 21:11:01 -0400

    Add files via upload

---
+++ Method1.java
@@ -0,0 +1,29 @@
+
+/**
+ * CSC 331 Programming Assignment 2
+ * Method 1
+ *
+ * @author Scott Spurlock
+ * @author William Reynolds IV
+ * @version 9/8/20
+ */
+public class Method1 {
+
+	/**
+	 * This method finds the k largest elements in an integer array
+	 * by looping through the array k times, each time picking the largest.
+	 * 
+	 * @param arr: input array to be searched
+	 * @param k: how many elements to select
+	 * @return an array of size k holding the largest k elements of A
+	 */
+	public int[] findTopK(int[] arr, int k) {
+		int[] results = new int[k];
+
+		// TBD
+		
+		return results;
+	}
+
+
+} // Method1
